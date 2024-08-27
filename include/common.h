e09867c65a8dc023e020989306c76ab7dbc7f253
Marceli Jankowski <marcelijankowski123@gmail.com>
2024-08-28 00:17:10 +0200

    Add P_S (Position Separator) object macro to common.h

--- include/common.h
+++ include/common.h
@@ -4,14 +4,15 @@
 #define STRINGIZE_HELPER(token) #token
 #define STRINGIZE(token) STRINGIZE_HELPER(token)
 
+#define COMMA ,
+#define M_S " - " // Message Separator
+#define P_S ":" // Position Separator
+
 #define FILE_FORMAT "%s"
 #define LINE_FORMAT "%d"
 #define COLUMN_FORMAT "%d"
-#define LINE_COLUMN_FORMAT LINE_FORMAT ":" COLUMN_FORMAT
-#define FILE_LINE_FORMAT FILE_FORMAT ":" LINE_FORMAT
-#define FILE_LINE_COLUMN_FORMAT FILE_FORMAT ":" LINE_COLUMN_FORMAT
-
-#define COMMA ,
-#define M_S " - " // Message Separator
+#define LINE_COLUMN_FORMAT LINE_FORMAT P_S COLUMN_FORMAT
+#define FILE_LINE_FORMAT FILE_FORMAT P_S LINE_FORMAT
+#define FILE_LINE_COLUMN_FORMAT FILE_FORMAT P_S LINE_COLUMN_FORMAT
 
 #endif // COMMON_H
