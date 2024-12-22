8465942d2064683db1931626552f30b74bc3b700
Marceli Jankowski <marcelijankowski123@gmail.com>
2024-12-22 18:13:54 +0100

    Add is_integer number utility

---
+++ include/utils/number.h
@@ -0,0 +1,8 @@
+#ifndef NUMBER_H
+#define NUMBER_H
+
+#include <stdbool.h>
+
+bool is_integer(double number);
+
+#endif // NUMBER_H
---
+++ src/utils/number.c
@@ -0,0 +1,10 @@
+#include "utils/number.h"
+
+#include <math.h>
+
+/**@desc determine whether `number` is an integer
+@return true if it is, false otherwise*/
+bool is_integer(double const number) {
+  if (isnan(number) || isinf(number)) return false;
+  return floor(number) == number;
+}
