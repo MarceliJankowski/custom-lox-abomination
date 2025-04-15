4618513d0c36198659f6dd78bb313755a785bb84
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-04-16 00:07:16 +0200

    Add software availability and version check to generate_compilation_database.sh script

--- scripts/miscellaneous/generate_compilation_database.sh
+++ scripts/miscellaneous/generate_compilation_database.sh
@@ -4,6 +4,13 @@ source "$(dirname "$0")/../common.sh"
 
 set -o errexit
 
+log_action "Checking required software availability and version conformance"
+if ! is_cmd_available "bear"; then
+  error "bear (tool for generating compilation database) is unavailable" $GENERIC_ERROR_CODE
+elif [[ $(bear --version) != "bear 3."* ]]; then
+  error "bear version doesn't conform to required '^3.0.0'" $GENERIC_ERROR_CODE
+fi
+
 log_action "Cleaning all cla builds"
 make clean 1>/dev/null
 
