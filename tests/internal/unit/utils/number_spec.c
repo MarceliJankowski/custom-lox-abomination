7aa93ad665c4c5a27e64854e3c16ed5a1fe2210b
uruun <urunn@tutanota.com>
2025-05-03 17:14:07 +0200

    Update

--- tests/internal/unit/utils/number_spec.c
+++ tests/internal/unit/utils/number_spec.c
@@ -1,41 +1,46 @@
 #include "unit/unit_test.h"
 #include "utils/number.h"
 
-#include <cmocka.h>
 #include <math.h>
 
+// *---------------------------------------------*
+// *                 TEST CASES                  *
+// *---------------------------------------------*
+
 static void number_is_integer__returns_true_for_integers(void **const _) {
-  const double positive = 1.0;
-  const double negative = -1.0;
-  const double zero = 0.0;
+  double const positive = 1.0;
+  double const negative = -1.0;
+  double const zero = 0.0;
 
   assert_true(number_is_integer(positive));
   assert_true(number_is_integer(negative));
   assert_true(number_is_integer(zero));
 }
 
-static void number_is_integer__returns_false_for_non_integer_numbers(void **const _) {
-  const double positive = 1.1;
-  const double negative = -1.1;
+static void number_is_integer__returns_false_for_floats(void **const _) {
+  double const positive = 1.1;
+  double const negative = -1.1;
 
   assert_false(number_is_integer(positive));
   assert_false(number_is_integer(negative));
 }
 
 static void number_is_integer__returns_false_for_nan(void **const _) {
-  const double nan = NAN;
+  double const nan = NAN;
+
   assert_false(number_is_integer(nan));
 }
 
 static void number_is_integer__returns_false_for_inf(void **const _) {
-  const double inf = INFINITY;
+  double const inf = INFINITY;
+
   assert_false(number_is_integer(inf));
 }
 
 int main(void) {
   struct CMUnitTest const tests[] = {
     cmocka_unit_test(number_is_integer__returns_true_for_integers),
-    cmocka_unit_test(number_is_integer__returns_false_for_non_integer_numbers),
+    cmocka_unit_test(number_is_integer__returns_false_for_floats),
     cmocka_unit_test(number_is_integer__returns_false_for_nan),
     cmocka_unit_test(number_is_integer__returns_false_for_inf)
   };
