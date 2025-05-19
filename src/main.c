994434d83f18d955d726cf9d95eccb0211b60802
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-05-19 16:52:16 +0200

    Rework cli/repl module

--- src/main.c
+++ src/main.c
@@ -9,14 +9,10 @@ int main(int const argc, char const *const *const argv) {
   g_bytecode_execution_error_stream = stderr;
   g_source_program_output_stream = stdout;
 
-  ErrorCode error_code = ERROR_CODE_SUCCESS;
-
   // process cli arguments
   g_source_file_path = args_process(argc, argv);
 
-  // run the interpreter in specified interaction mode
+  // run interpreter in the specified interaction mode
   if (g_source_file_path == NULL) repl_enter();
-  else error_code = file_interpret(g_source_file_path);
-
-  return error_code;
+  return file_interpret(g_source_file_path);
 }
