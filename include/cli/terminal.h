8e74694e963b0d09cd5c8fb7cc55a8d6f0b58b05
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-05-20 18:41:59 +0200

    Update terminal_enable_noncannonical_mode to handle stdin not being connected to a terminal

--- include/cli/terminal.h
+++ include/cli/terminal.h
@@ -1,14 +1,17 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
+#include <stdbool.h>
+
 // *---------------------------------------------*
 // *             FUNCTION PROTOTYPES             *
 // *---------------------------------------------*
 
-/**@desc enable noncannonical terminal mode.
+/**@desc enable noncannonical terminal mode (unless stdin is not connected to a terminal).
 Once enabled, it persists until process termination.
 This function registers handlers for terminal parameter restoration upon imminent process termination.
-Only single invocation is permitted.*/
-void terminal_enable_noncannonical_mode(void);
+Invocations are permitted until noncannonical terminal mode gets enabled.
+@return true if noncannonical terminal mode was enabled, false otherwise (stdin is not connected to a terminal)*/
+bool terminal_enable_noncannonical_mode(void);
 
 #endif // TERMINAL_H
