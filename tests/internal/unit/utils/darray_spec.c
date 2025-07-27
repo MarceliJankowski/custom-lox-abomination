c3501fbe0ef0b24eff4183fc96732b5b5de3efd2
Mikolaj Turski <mturski@protonmail.com>
2025-07-27 09:15:00 +0200

    Use int instead of int32_t

--- tests/internal/unit/utils/darray_spec.c
+++ tests/internal/unit/utils/darray_spec.c
@@ -6,21 +6,21 @@
 #include <utils/darray.h>
 
 static void darray_define__default_values(void **const _) {
-  DARRAY_DEFINE(int32_t, test_darray, memory_manage);
+  DARRAY_DEFINE(int, test_darray, memory_manage);
 
   assert_true(test_darray.memory_manager == memory_manage);
   assert_true(test_darray.capacity == 0);
   assert_true(test_darray.count == 0);
   assert_true(test_darray.capacity_growth_factor == DARRAY_DEFAULT_CAPACITY_GROWTH_FACTOR);
   assert_true(test_darray.min_growth_capacity == DARRAY_DEFAULT_MIN_GROWTH_CAPACITY);
-  assert_true(test_darray.data_object_size == sizeof(int32_t));
+  assert_true(test_darray.data_object_size == sizeof(int));
   assert_true(test_darray.data == NULL);
 
   DARRAY_FREE(&test_darray);
 }
 
 static void darray_grow_capacity__multiple_calls(void **const _) {
-  DARRAY_DEFINE(int32_t, test_darray, memory_manage);
+  DARRAY_DEFINE(int, test_darray, memory_manage);
 
   assert_true(test_darray.capacity == 0);
 
@@ -34,12 +34,12 @@ static void darray_grow_capacity__multiple_calls(void **const _) {
 }
 
 static void darray_append__append_once(void **const _) {
-  DARRAY_DEFINE(int32_t, test_darray, memory_manage);
+  DARRAY_DEFINE(int, test_darray, memory_manage);
 
-  int32_t const element = 1;
+  int const element = 1;
 
   DARRAY_APPEND(&test_darray, element);
-  int32_t const result = test_darray.data[0];
+  int const result = test_darray.data[0];
 
   assert_true(result == 1);
 
