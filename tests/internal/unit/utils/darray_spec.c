482cb2c3d44601d452b8da6afe96ccf62692d071
Mikolaj Turski <mturski@protonmail.com>
2025-07-27 10:16:19 +0200

    Add more darray test cases

--- tests/internal/unit/utils/darray_spec.c
+++ tests/internal/unit/utils/darray_spec.c
@@ -19,9 +19,25 @@ static void darray_define__default_values(void **const _) {
   DARRAY_FREE(&test_darray);
 }
 
-static void darray_grow_capacity__multiple_calls(void **const _) {
+static void daray_free__free_with_data(void **const _) {
   DARRAY_DEFINE(int, test_darray, memory_manage);
 
+  int const element = 1;
+  DARRAY_APPEND(&test_darray, element);
+
+  DARRAY_FREE(&test_darray);
+
+  assert_true(test_darray.memory_manager == memory_manage);
+  assert_true(test_darray.capacity == 0);
+  assert_true(test_darray.count == 0);
+  assert_true(test_darray.capacity_growth_factor == DARRAY_DEFAULT_CAPACITY_GROWTH_FACTOR);
+  assert_true(test_darray.min_growth_capacity == DARRAY_DEFAULT_MIN_GROWTH_CAPACITY);
+  assert_true(test_darray.data_object_size == sizeof(int));
+  assert_true(test_darray.data == NULL);
+}
+
+static void darray_grow_capacity__multiple_calls(void **const _) {
+  DARRAY_DEFINE(int, test_darray, memory_manage);
   assert_true(test_darray.capacity == 0);
 
   DARRAY_GROW_CAPACITY(&test_darray);
@@ -36,21 +52,43 @@ static void darray_grow_capacity__multiple_calls(void **const _) {
 static void darray_append__append_once(void **const _) {
   DARRAY_DEFINE(int, test_darray, memory_manage);
 
-  int const element = 1;
+  assert_true(test_darray.capacity == 0);
 
+  int const element = 1;
   DARRAY_APPEND(&test_darray, element);
   int const result = test_darray.data[0];
 
   assert_true(result == 1);
+  assert_true(test_darray.capacity == 8);
+
+  DARRAY_FREE(&test_darray);
+}
+
+static void darray_append__append_multiple(void **const _) {
+  DARRAY_DEFINE(int, test_darray, memory_manage);
+
+  int const element1 = 1;
+  DARRAY_APPEND(&test_darray, element1);
+  int const result1 = test_darray.data[0];
+
+  assert_true(result1 == 1);
+  assert_true(test_darray.capacity == 8);
+
+  int const element2 = 2;
+  DARRAY_APPEND(&test_darray, element2);
+  int const result2 = test_darray.data[1];
+
+  assert_true(result2 == 2);
+  assert_true(test_darray.capacity == 8);
 
   DARRAY_FREE(&test_darray);
 }
 
 int main(void) {
   struct CMUnitTest const tests[] = {
-    cmocka_unit_test(darray_define__default_values),
-    cmocka_unit_test(darray_grow_capacity__multiple_calls),
-    cmocka_unit_test(darray_append__append_once),
+    cmocka_unit_test(darray_define__default_values),        cmocka_unit_test(daray_free__free_with_data),
+    cmocka_unit_test(darray_grow_capacity__multiple_calls), cmocka_unit_test(darray_append__append_once),
+    cmocka_unit_test(darray_append__append_multiple),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
