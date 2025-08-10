cbd5db3d0274bac3d095ab3b0d897dd4b3faf629
Mikolaj Turski <mturski@protonmail.com>
2025-08-10 17:49:42 +0200

    Add memory_reallocate and memory_deallocate tests

--- tests/internal/unit/utils/memory_spec.c
+++ tests/internal/unit/utils/memory_spec.c
@@ -41,10 +41,38 @@ static void memory_allocate__test(void **const _) {
   char i = 1;
   char *p = &i;
   will_return(__wrap_realloc, p);
-  char *ptr = (char *)memory_allocate(memory_manage, sizeof(char));
 
+  char *ptr = (char *)memory_allocate(memory_manage, sizeof(char));
   assert_ptr_equal(ptr, p);
-  assert_non_null(ptr);
+}
+
+static void memory_reallocate__test(void **const _) {
+  expect_value(__wrap_realloc, __size, sizeof(char));
+  expect_value(__wrap_realloc, __ptr, NULL);
+  char i = 1;
+  char *p = &i;
+  will_return(__wrap_realloc, p);
+
+  char *ptr = (char *)memory_allocate(memory_manage, sizeof(char));
+  assert_ptr_equal(ptr, p);
+
+  expect_value(__wrap_realloc, __size, sizeof(char) * 2);
+  expect_value(__wrap_realloc, __ptr, p);
+  will_return(__wrap_realloc, p);
+
+  char *ptr2 = (char *)memory_reallocate(memory_manage, p, sizeof(char), sizeof(char) * 2);
+  assert_ptr_equal(ptr2, p);
+}
+
+static void memory_deallocate__test(void **const _) {
+  expect_value(__wrap_realloc, __size, sizeof(char));
+  expect_value(__wrap_realloc, __ptr, NULL);
+  char i = 1;
+  char *p = &i;
+  will_return(__wrap_realloc, p);
+
+  char *ptr = (char *)memory_allocate(memory_manage, sizeof(char));
+  assert_ptr_equal(ptr, p);
 
   expect_value(__wrap_free, __ptr, ptr);
   memory_deallocate(memory_manage, ptr, sizeof(char));
@@ -53,7 +81,10 @@ static void memory_allocate__test(void **const _) {
 int main(void) {
   struct CMUnitTest const tests[] = {
     cmocka_unit_test(get_byte__retrieves_bytes_in_LSB_to_MSB_order),
-    cmocka_unit_test(concatenate_bytes__concatenates_bytes_in_MSB_to_LSB_order), cmocka_unit_test(memory_allocate__test)
+    cmocka_unit_test(concatenate_bytes__concatenates_bytes_in_MSB_to_LSB_order),
+    cmocka_unit_test(memory_allocate__test),
+    cmocka_unit_test(memory_reallocate__test),
+    cmocka_unit_test(memory_deallocate__test),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
