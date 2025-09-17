675fa72849c3f67a8b430db94d86b3084e61d294
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-09-17 23:54:11 +0200

    Rework REPL history navigation

--- include/cli/history.h
+++ include/cli/history.h
@@ -11,11 +11,10 @@
 void history_init(void);
 void history_destroy(void);
 void history_append_entry(char const *entry, size_t entry_length);
-void history_browse_older_entry(void);
-void history_browse_newer_entry(void);
 void history_stop_browsing(void);
+char const *history_browse_older_entry(void);
+char const *history_browse_newer_entry(void);
 bool history_is_browsed(void);
 bool history_is_newest_entry_browsed(void);
-char const *history_get_browsed_entry(void);
 
 #endif // HISTORY_H
