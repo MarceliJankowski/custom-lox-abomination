57619470f92a2a28c5d1e511e30f6ba9e50912a5
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-10-21 23:34:27 +0200

    Add character_is_alphanumeric utility

--- src/utils/character.c
+++ src/utils/character.c
@@ -6,3 +6,4 @@
 
 bool character_is_whitespace(char character);
 bool character_is_digit(char character);
+bool character_is_alphanumeric(char character);
--- tests/internal/unit/utils/character_spec.c
+++ tests/internal/unit/utils/character_spec.c
@@ -9,6 +9,11 @@
 
 static char const whitespace_characters[] = {' ', '\n', '\f', '\t', '\v', '\r'};
 static char const digit_characters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+static char const alphanumeric_characters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 // *---------------------------------------------*
 // *                 TEST CASES                  *
@@ -66,12 +71,40 @@ static void character_is_digit__returns_false_for_non_digit_characters(void **co
   }
 }
 
+static void character_is_alphanumeric__returns_true_for_alphanumeric_characters(void **const _) {
+  for (size_t i = 0; i < sizeof(alphanumeric_characters); i++) {
+    char const alphanumeric_character = alphanumeric_characters[i];
+
+    bool const result = character_is_alphanumeric(alphanumeric_character);
+
+    assert_true(result);
+  }
+}
+
+static void character_is_alphanumeric__returns_false_for_non_alphanumeric_characters(void **const _) {
+  for (int character = CHAR_MIN; character <= CHAR_MAX; character++) {
+    // skip alphanumeric characters
+    for (size_t i = 0; i < sizeof(alphanumeric_characters); i++) {
+      char const alphanumeric_character = alphanumeric_characters[i];
+      if (character == alphanumeric_character) goto skip_alphanumeric_character;
+    }
+
+    bool const result = character_is_alphanumeric(character);
+
+    assert_false(result);
+
+  skip_alphanumeric_character:;
+  }
+}
+
 int main(void) {
   struct CMUnitTest const tests[] = {
     cmocka_unit_test(character_is_whitespace__returns_true_for_whitespace_characters),
     cmocka_unit_test(character_is_whitespace__returns_false_for_non_whitespace_characters),
     cmocka_unit_test(character_is_digit__returns_true_for_digit_characters),
     cmocka_unit_test(character_is_digit__returns_false_for_non_digit_characters),
+    cmocka_unit_test(character_is_alphanumeric__returns_true_for_alphanumeric_characters),
+    cmocka_unit_test(character_is_alphanumeric__returns_false_for_non_alphanumeric_characters),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
