cebffcf5f3abc6c1fdfa1dfbde7e0102d68dddc9
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-10-22 00:03:52 +0200

    Refactor character_spec module

--- tests/internal/unit/utils/character_spec.c
+++ tests/internal/unit/utils/character_spec.c
@@ -4,107 +4,107 @@
 #include <limits.h>
 
 // *---------------------------------------------*
-// *          INTERNAL-LINKAGE OBJECTS           *
+// *              MACRO DEFINITIONS              *
 // *---------------------------------------------*
 
-static char const whitespace_characters[] = {' ', '\n', '\f', '\t', '\v', '\r'};
-static char const digit_characters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-static char const alphanumeric_characters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                                               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                                               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                                               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+#define ASSERT_FN_RETURNS_TRUE_FOR_TEST_STATE(fn)              \
+  do {                                                         \
+    TestState const *const test_state = (TestState *)(*state); \
+                                                               \
+    for (size_t i = 0; i < test_state->count; i++) {           \
+      char const character = test_state->characters[i];        \
+                                                               \
+      bool const result = fn(character);                       \
+                                                               \
+      assert_true(result);                                     \
+    }                                                          \
+  } while (0)
+
+#define ASSERT_FN_RETURNS_FALSE_FOR_TEST_STATE(fn)                       \
+  do {                                                                   \
+    TestState const *const test_state = (TestState *)(*state);           \
+                                                                         \
+    for (int character = CHAR_MIN; character <= CHAR_MAX; character++) { \
+      /* skip test_state characters */                                   \
+      for (size_t i = 0; i < test_state->count; i++) {                   \
+        char const test_state_character = test_state->characters[i];     \
+        if (character == test_state_character) goto skip_character;      \
+      }                                                                  \
+                                                                         \
+      bool const result = fn(character);                                 \
+                                                                         \
+      assert_false(result);                                              \
+                                                                         \
+    skip_character:;                                                     \
+    }                                                                    \
+  } while (0)
+
+#define MAKE_TEST_STATE(character_array) ((TestState){(character_array), sizeof((character_array))})
+
+// *---------------------------------------------*
+// *              TYPE DEFINITIONS               *
+// *---------------------------------------------*
+
+typedef struct {
+  char const *characters;
+  size_t count;
+} TestState;
 
 // *---------------------------------------------*
 // *                 TEST CASES                  *
 // *---------------------------------------------*
 
-static void character_is_whitespace__returns_true_for_whitespace_characters(void **const _) {
-  for (size_t i = 0; i < sizeof(whitespace_characters); i++) {
-    char const whitespace_character = whitespace_characters[i];
-
-    bool const result = character_is_whitespace(whitespace_character);
-
-    assert_true(result);
-  }
+static void character_is_whitespace__returns_true_for_whitespace_characters(void **const state) {
+  ASSERT_FN_RETURNS_TRUE_FOR_TEST_STATE(character_is_whitespace);
 }
 
-static void character_is_whitespace__returns_false_for_non_whitespace_characters(void **const _) {
-  for (int character = CHAR_MIN; character <= CHAR_MAX; character++) {
-    // skip whitespace characters
-    for (size_t i = 0; i < sizeof(whitespace_characters); i++) {
-      char const whitespace_character = whitespace_characters[i];
-      if (character == whitespace_character) goto skip_whitespace_character;
-    }
-
-    bool const result = character_is_whitespace(character);
-
-    assert_false(result);
-
-  skip_whitespace_character:;
-  }
+static void character_is_whitespace__returns_false_for_non_whitespace_characters(void **const state) {
+  ASSERT_FN_RETURNS_FALSE_FOR_TEST_STATE(character_is_whitespace);
 }
 
-static void character_is_digit__returns_true_for_digit_characters(void **const _) {
-  for (size_t i = 0; i < sizeof(digit_characters); i++) {
-    char const digit_character = digit_characters[i];
-
-    bool const result = character_is_digit(digit_character);
-
-    assert_true(result);
-  }
+static void character_is_digit__returns_true_for_digit_characters(void **const state) {
+  ASSERT_FN_RETURNS_TRUE_FOR_TEST_STATE(character_is_digit);
 }
 
-static void character_is_digit__returns_false_for_non_digit_characters(void **const _) {
-  for (int character = CHAR_MIN; character <= CHAR_MAX; character++) {
-    // skip digit characters
-    for (size_t i = 0; i < sizeof(digit_characters); i++) {
-      char const digit_character = digit_characters[i];
-      if (character == digit_character) goto skip_digit_character;
-    }
-
-    bool const result = character_is_digit(character);
-
-    assert_false(result);
-
-  skip_digit_character:;
-  }
+static void character_is_digit__returns_false_for_non_digit_characters(void **const state) {
+  ASSERT_FN_RETURNS_FALSE_FOR_TEST_STATE(character_is_digit);
 }
 
-static void character_is_alphanumeric__returns_true_for_alphanumeric_characters(void **const _) {
-  for (size_t i = 0; i < sizeof(alphanumeric_characters); i++) {
-    char const alphanumeric_character = alphanumeric_characters[i];
-
-    bool const result = character_is_alphanumeric(alphanumeric_character);
-
-    assert_true(result);
-  }
+static void character_is_alphanumeric__returns_true_for_alphanumeric_characters(void **const state) {
+  ASSERT_FN_RETURNS_TRUE_FOR_TEST_STATE(character_is_alphanumeric);
 }
 
-static void character_is_alphanumeric__returns_false_for_non_alphanumeric_characters(void **const _) {
-  for (int character = CHAR_MIN; character <= CHAR_MAX; character++) {
-    // skip alphanumeric characters
-    for (size_t i = 0; i < sizeof(alphanumeric_characters); i++) {
-      char const alphanumeric_character = alphanumeric_characters[i];
-      if (character == alphanumeric_character) goto skip_alphanumeric_character;
-    }
-
-    bool const result = character_is_alphanumeric(character);
-
-    assert_false(result);
-
-  skip_alphanumeric_character:;
-  }
+static void character_is_alphanumeric__returns_false_for_non_alphanumeric_characters(void **const state) {
+  ASSERT_FN_RETURNS_FALSE_FOR_TEST_STATE(character_is_alphanumeric);
 }
 
 int main(void) {
+  char const whitespace_characters[] = {' ', '\n', '\f', '\t', '\v', '\r'};
+  TestState whitespace_state = MAKE_TEST_STATE(whitespace_characters);
+
+  char const digit_characters[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  TestState digit_state = MAKE_TEST_STATE(digit_characters);
+
+  size_t const alphanumeric_character_count = ('z' - 'a' + 1) + ('Z' - 'A' + 1) + ('9' - '0' + 1);
+  char alphanumeric_characters[alphanumeric_character_count];
+  {
+    size_t i = 0;
+    for (char c = 'a'; c <= 'z'; i++, c++) alphanumeric_characters[i] = c;
+    for (char c = 'A'; c <= 'Z'; i++, c++) alphanumeric_characters[i] = c;
+    for (char c = '0'; c <= '9'; i++, c++) alphanumeric_characters[i] = c;
+    assert(i == alphanumeric_character_count);
+  }
+  TestState alphanumeric_state = MAKE_TEST_STATE(alphanumeric_characters);
+
   struct CMUnitTest const tests[] = {
-    cmocka_unit_test(character_is_whitespace__returns_true_for_whitespace_characters),
-    cmocka_unit_test(character_is_whitespace__returns_false_for_non_whitespace_characters),
-    cmocka_unit_test(character_is_digit__returns_true_for_digit_characters),
-    cmocka_unit_test(character_is_digit__returns_false_for_non_digit_characters),
-    cmocka_unit_test(character_is_alphanumeric__returns_true_for_alphanumeric_characters),
-    cmocka_unit_test(character_is_alphanumeric__returns_false_for_non_alphanumeric_characters),
+    cmocka_unit_test_prestate(character_is_whitespace__returns_true_for_whitespace_characters, &whitespace_state),
+    cmocka_unit_test_prestate(character_is_whitespace__returns_false_for_non_whitespace_characters, &whitespace_state),
+    cmocka_unit_test_prestate(character_is_digit__returns_true_for_digit_characters, &digit_state),
+    cmocka_unit_test_prestate(character_is_digit__returns_false_for_non_digit_characters, &digit_state),
+    cmocka_unit_test_prestate(character_is_alphanumeric__returns_true_for_alphanumeric_characters, &alphanumeric_state),
+    cmocka_unit_test_prestate(
+      character_is_alphanumeric__returns_false_for_non_alphanumeric_characters, &alphanumeric_state
+    ),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
