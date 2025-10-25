354b66c8284acfed6d0c19105f33facd25b11288
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-10-26 00:18:00 +0200

    Update REPL to exit with 0 on Ctrl-D shortcut

--- src/cli/terminal.c
+++ src/cli/terminal.c
@@ -290,6 +290,9 @@ TerminalKey terminal_read_key(void) {
 
   // handle control sequence characters
   switch (char_1) {
+    case 0x4: { // EOT
+      return MAKE_CONTROL_KEY(TERMINAL_KEY_EOF);
+    }
     case 0x7F: { // DEL
       return MAKE_CONTROL_KEY(TERMINAL_KEY_BACKSPACE);
     }
