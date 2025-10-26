170e36c478c1543edecff4b3f1a0cb6a15e86e7d
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-10-27 00:17:32 +0100

    Add Alt-F shortcut support to REPL

--- src/cli/repl.c
+++ src/cli/repl.c
@@ -120,7 +120,7 @@ void repl_enter(void) {
       TerminalKeyType const key_type = TERMINAL_KEY_GET_TYPE(key);
 
       // handle key
-      static_assert(TERMINAL_KEY_TYPE_COUNT == 19, "Exhaustive TerminalKeyType handling");
+      static_assert(TERMINAL_KEY_TYPE_COUNT == 20, "Exhaustive TerminalKeyType handling");
       switch (key_type) {
         case TERMINAL_KEY_PRINTABLE: {
           if (key.printable.character == '\n') {
@@ -174,6 +174,7 @@ void repl_enter(void) {
           gap_buffer_move_cursor_right_by_char(&physical_line);
           break;
         }
+        case TERMINAL_KEY_ALT_F:
         case TERMINAL_KEY_CTRL_ARROW_RIGHT: {
           gap_buffer_move_cursor_right_by_word(&physical_line);
           break;
