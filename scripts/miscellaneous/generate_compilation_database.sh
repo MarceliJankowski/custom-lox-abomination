590b936ff07bcfdee411a460acf626fa35cf3e22
Marceli Jankowski <marcelijankowski123@gmail.com>
2026-02-09 23:12:36 +0100

    Update required bear version to ^4.0.0

--- scripts/miscellaneous/generate_compilation_database.sh
+++ scripts/miscellaneous/generate_compilation_database.sh
@@ -7,8 +7,8 @@ set -o errexit
 log_action "Checking required software availability and version conformance"
 if ! is_cmd_available "bear"; then
   error "bear (tool for generating compilation database) is unavailable" $GENERIC_ERROR_CODE
-elif [[ $(bear --version) != "bear 3."* ]]; then
-  error "bear version doesn't conform to required '^3.0.0'" $GENERIC_ERROR_CODE
+elif [[ $(bear --version) != "bear 4."* ]]; then
+  error "bear version doesn't conform to required '^4.0.0'" $GENERIC_ERROR_CODE
 fi
 
 log_action "Cleaning all cla builds"
