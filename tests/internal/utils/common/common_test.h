faacfd7efc2624a4ed356c119f9b86770e0a3ee4
Marceli Jankowski <marcelijankowski123@gmail.com>
2026-02-09 23:25:03 +0100

    Update required cmocka version to ^2.0.0

--- tests/internal/utils/common/common_test.h
+++ tests/internal/utils/common/common_test.h
@@ -2,13 +2,6 @@
 #define COMMON_TEST_H
 
 #include <assert.h>
-#include <setjmp.h>
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdint.h>
-
-// clang-format off
 #include <cmocka.h>
-// clang-format on
 
 #endif // COMMON_TEST_H
